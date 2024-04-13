a84fd9b7fa120ac12b2d3fba28db94905dc8dada
Pauline Ann Bautista <paulineannbautista@gmail.com>
2024-04-13 22:05:13 +0800

    updated navigation bar function

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-console.log("tester")
-
 let nav = document.querySelector("nav");
 window.addEventListener("scroll", function() {
-    console.log("tester2")
   if (window.scrollY > 20) {
     nav.classList.add("sticky");
     console.log("tester3")
@@ -11,3 +8,37 @@ window.addEventListener("scroll", function() {
   }
 });
 
+
+let navLinks = document.querySelectorAll('.linksNavig a');
+
+function getOffsets() {
+  let offsets = [];
+  navLinks.forEach(function(link) {
+    let targetId = link.getAttribute('href').substring(1);
+    let targetElement = document.getElementById(targetId);
+    let offset = targetElement.offsetTop;
+    offsets.push(offset);
+  });
+  return offsets;
+}
+
+function updateNavigation() {
+  let offsets = getOffsets();
+  let scrollPosition = window.scrollY;
+
+  offsets.forEach(function(offset, index) {
+    if (scrollPosition >= offset) {
+      navLinks.forEach(function(link) {
+        link.classList.remove('selected');
+      });
+      navLinks[index].classList.add('selected');
+    }
+  });
+}
+
+window.addEventListener('scroll', function() {
+  updateNavigation();
+});
+
+
+updateNavigation();
